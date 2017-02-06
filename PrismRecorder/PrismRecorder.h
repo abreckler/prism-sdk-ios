a92c70c4877b4dda37f3bfafdef30837ca65acb7
Ahmed BOUCHFAA <abouchfaa@gmail.com>
2017-02-06 10:55:24 +0000

    Initial Commit

---
+++ PrismRecorder/PrismRecorder.h
@@ -0,0 +1,19 @@
+//
+//  PrismRecorder.h
+//  PrismRecorder
+//
+//  Created by Ahmed Bouchfaa on 2/6/17.
+//  Copyright © 2017 prism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrismRecorder.
+FOUNDATION_EXPORT double PrismRecorderVersionNumber;
+
+//! Project version string for PrismRecorder.
+FOUNDATION_EXPORT const unsigned char PrismRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrismRecorder/PublicHeader.h>
+
+
