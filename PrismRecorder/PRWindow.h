4635005dbf9d97cf403fe7f15b648d811b55a6ef
Ahmed BOUCHFAA <abouchfaa@gmail.com>
2017-02-08 10:57:14 +0000

    added window subclass

---
+++ PrismRecorder/PRWindow.h
@@ -0,0 +1,13 @@
+//
+//  PRWindow.h
+//  PrismRecorder
+//
+//  Created by Ahmed Bouchfaa on 2/8/17.
+//  Copyright © 2017 prism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRWindow : UIWindow
+
+@end
